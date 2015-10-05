e44747324a224cc6d19e343a03486966f8bc3590
Jaisiel Santana Almeida <jaisiel@gmail.com>
2015-10-05 13:37:51 +0100

    trying to instal wkhtmltopdf with sudo privilegies

--- travis/wkhtmltopdf_installation.sh
+++ travis/wkhtmltopdf_installation.sh
@@ -1,1 +1,1 @@
-wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb && dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb && sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
