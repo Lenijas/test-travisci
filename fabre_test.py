6735708a3abb233cbfffc99f3d0efe40e7ac1554
Jaisiel Santana Almeida <jaisiel@gmail.com>
2015-10-05 11:13:58 +0100

    test triggering travis

--- fabre_test.py
+++ fabre_test.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
 # coding=UTF-8
-import pytest
 import sys
 
-
-# content of test_assert1.py
-def f():
-    return 3
-
-def test_function():
-    assert f() == 4
-
-
-test_function()
+sys.exit(0)
